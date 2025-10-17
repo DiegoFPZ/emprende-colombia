c1d9b45f599e9ec7d4b20025bc11fd9693755297
jorge <jhmorenof@gmail.com>
2025-10-16 21:50:18 -0500

    Este es mi primer commit con la configuración corregida

---
+++ js/algoritmo.js
@@ -0,0 +1,12 @@
+
+function calcula(){
+let rp1=parseInt(document.getElementById("Pregunta1").value);
+let rp2=parseInt(document.getElementById("Pregunta2").value);
+let rp3=parseInt(document.getElementById("Pregunta3").value);
+let rp4=parseInt(document.getElementById("Pregunta4").value);
+//luego se suma, se calcula el puntaje
+if categoria
+
+if
+
+}
